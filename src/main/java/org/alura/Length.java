e8bdeb64ea7a2cbc9442cd310961d6787eeb8895
Alfonso-Rangel <luisrangel187@hotmail.com>
2023-07-10 19:23:18 -0600

    Se ha completado 4/5 conversiones, la moneda es la faltante.

--- src/main/java/org/alura/Length.java
+++ src/main/java/org/alura/Length.java
@@ -1,14 +1,14 @@
 package org.alura;
 
 public enum Length {
-    MILLIMETERS("millimeters", 0.001),
-    CENTIMETERS("centimeters", 0.01),
-    METERS("meters", 1.0),
-    KILOMETERS("kilometers", 1000.0),
-    INCHES("inches", 0.0254),
-    FEET("feet", 0.3048),
-    YARDS("yards", 0.9144),
-    MILES("miles", 1609.34);
+    MILLIMETERS("Millimeters", 0.001),
+    CENTIMETERS("Centimeters", 0.01),
+    METERS("Meters", 1.0),
+    KILOMETERS("Kilometers", 1000.0),
+    INCHES("Inches", 0.0254),
+    FEET("Feet", 0.3048),
+    YARDS("Yards", 0.9144),
+    MILES("Miles", 1609.34);
 
     private final String name;
     private final double value;
@@ -24,5 +24,19 @@ public enum Length {
 
     public double getValue() {
         return value;
+    }
+
+    static Length getValueOption(String option) {
+        return switch (option) {
+            case "Millimeters" -> Length.MILLIMETERS;
+            case "Centimeters" -> Length.CENTIMETERS;
+            case "Meters" -> Length.METERS;
+            case "Kilometers" -> Length.KILOMETERS;
+            case "Inches" -> Length.INCHES;
+            case "Feet" -> Length.FEET;
+            case "Yards" -> Length.YARDS;
+            case "Miles" -> Length.MILES;
+            default -> Length.MILLIMETERS;
+        };
     }
 }
--- src/main/java/org/alura/MainFrame.java
+++ src/main/java/org/alura/MainFrame.java
@@ -1,18 +1,29 @@
 package org.alura;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
     private JPanel mainPanel;
     private JPanel menuPanel;
     private JButton selectedButton;
+    private String mode = "Concurrency";
+    private String unitSource = "";
+    private String unitDestiny = "";
     private final Color resaltado = Color.decode("#FF666F");
     private final Color resaltadoAcento = Color.decode("#333333");
     private final Color defecto = Color.decode("#293840");
     private final Color defectoAcento = Color.decode("#A4AABB");
-
-    private final String[] devices = new String[]{"Dollar", "Euro", "Peso"};
+    private final String[] currency; {
+        currency = new String[Currency.values().length];
+        int i = 0;
+        for (Currency unit : Currency.values()) {
+            currency[i] = unit.getName();
+            i++;
+        }
+    }
     private final String[] lengthUnit; {
         lengthUnit = new String[Length.values().length];
         int i = 0;
@@ -22,9 +33,30 @@ public class MainFrame extends JFrame {
         }
     }
 
-    private final String[] time = new String[]{"Seconds", "Minutes", "Days"};
-    private final String[] tempeture = new String[]{"Centigrade", "Kelvin", "Fahrenheit"};
-    private final String[] data = new String[]{"Bit", "Megabit", "Gigabit"};
+    private final String[] time; {
+        time = new String[Time.values().length];
+        int i = 0;
+        for (Time unit : Time.values()) {
+            time[i] = unit.getName();
+            i++;
+        }
+    }
+    private final String[] temperature; {
+        temperature = new String[Temperature.values().length];
+        int i = 0;
+        for (Temperature unit : Temperature.values()) {
+            temperature[i] = unit.getName();
+            i++;
+        }
+    }
+    private final String[] data; {
+        data = new String[Data.values().length];
+        int i = 0;
+        for (Data unit : Data.values()) {
+            data[i] = unit.getName();
+            i++;
+        }
+    };
 
     public MainFrame() {
         setTitle("Conversor de Unidades");
@@ -33,7 +65,7 @@ public class MainFrame extends JFrame {
 
         createSelectorMenu();
         selectDefaultButton(); // Llama al método para seleccionar el primer botón por defecto
-        createConversorPanel(devices);
+        createConversorPanel(currency);
 
         pack();
         setLocationRelativeTo(null);
@@ -48,19 +80,19 @@ public class MainFrame extends JFrame {
         logoLabel.setPreferredSize(new Dimension(180, 100));
         menuPanel.add(logoLabel);
 
-        JButton button1 = createButtonMenu("Moneda", "multimedia/monedas.png", devices);
+        JButton button1 = createButtonMenu("Currency", "multimedia/monedas.png", currency);
         menuPanel.add(button1);
 
-        JButton button2 = createButtonMenu("Tiempo", "multimedia/tiempo.png", time);
+        JButton button2 = createButtonMenu("Time", "multimedia/tiempo.png", time);
         menuPanel.add(button2);
 
-        JButton button3 = createButtonMenu("Longitud", "multimedia/longitud.png", lengthUnit);
+        JButton button3 = createButtonMenu("Length", "multimedia/longitud.png", lengthUnit);
         menuPanel.add(button3);
 
-        JButton button4 = createButtonMenu("Temperatura", "multimedia/temperatura.png", tempeture);
+        JButton button4 = createButtonMenu("Temperature", "multimedia/temperatura.png", temperature);
         menuPanel.add(button4);
 
-        JButton button5 = createButtonMenu("Datos", "multimedia/datos.png", data);
+        JButton button5 = createButtonMenu("Data", "multimedia/datos.png", data);
         menuPanel.add(button5);
 
         add(menuPanel, BorderLayout.WEST);
@@ -93,6 +125,7 @@ public class MainFrame extends JFrame {
             createConversorPanel(unit);
             mainPanel.revalidate();
             mainPanel.repaint();
+            mode = button.getText();
         });
         return button;
     }
@@ -111,8 +144,46 @@ public class MainFrame extends JFrame {
         mainPanel = new JPanel();
         mainPanel.setLayout(new GridLayout(3, 2, 10, 40));
 
-        JPanel origenPanel = designConversorPanel("Valor de origen: ", new JTextField(), new JComboBox<>(options));
-        JPanel destinoPanel = designConversorPanel("Valor destino: ", new JTextField(), new JComboBox<>(options));
+        JTextField origenTextField = new JTextField();
+        JComboBox<String> origenComboBox = new JComboBox<>(options);
+        JPanel origenPanel = designConversorPanel("Valor de origen: ", origenTextField, origenComboBox);
+
+        JTextField destinoTextField = new JTextField();
+        destinoTextField.setEditable(false);
+        JComboBox<String> destinoComboBox = new JComboBox<>(options);
+        JPanel destinoPanel = designConversorPanel("Valor destino: ", destinoTextField, destinoComboBox);
+
+
+        // Guardar la opción seleccionada del JComboBox para el campo de origen
+        origenComboBox.addActionListener(e -> {
+            unitSource = (String) origenComboBox.getSelectedItem();
+            SwingUtilities.invokeLater(() -> {
+                destinoTextField.setText(convertUnits(origenTextField.getText()));
+            });
+        });
+
+        // Guardar la opción seleccionada del JComboBox para el campo de destino
+        destinoComboBox.addActionListener(e -> {
+            unitDestiny = (String) destinoComboBox.getSelectedItem();
+            SwingUtilities.invokeLater(() -> {
+                destinoTextField.setText(convertUnits(origenTextField.getText()));
+            });
+        });
+
+        // Agregar DocumentListener al campo de origen
+        origenTextField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {updateDestinoTextField();}
+            @Override
+            public void removeUpdate(DocumentEvent e) {updateDestinoTextField();}
+            @Override
+            public void changedUpdate(DocumentEvent e) {updateDestinoTextField();}
+            private void updateDestinoTextField() {
+                SwingUtilities.invokeLater(() -> {
+                    destinoTextField.setText(convertUnits(origenTextField.getText()));
+                });
+            }
+        });
 
         mainPanel.add(origenPanel);
         mainPanel.add(destinoPanel);
@@ -120,11 +191,11 @@ public class MainFrame extends JFrame {
         JLabel infoLabel = new JLabel("PLACEHOLDER");
         mainPanel.add(infoLabel);
 
-        // Agregar espacio vertical entre los paneles
         mainPanel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
 
         add(mainPanel, BorderLayout.CENTER);
     }
+
 
     private JPanel designConversorPanel(String label, JTextField textField, JComboBox<String> comboBox) {
         JPanel panel = new JPanel(new BorderLayout());
@@ -135,5 +206,43 @@ public class MainFrame extends JFrame {
         panel.add(textField, BorderLayout.CENTER);
         panel.add(comboBox, BorderLayout.SOUTH);
         return panel;
+    }
+
+    private String convertUnits(String input) {
+        double value;
+        try {
+            value = Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            return input;
+        }
+        switch (mode) {
+            case "Currency" -> {
+                Currency unit1 = Currency.getValueOption(unitSource);
+                Currency unit2 = Currency.getValueOption(unitDestiny);
+                value = (Conversor.convert(unit1, unit2, value));
+            }
+            case "Length" -> {
+                Length unit1 = Length.getValueOption(unitSource);
+                Length unit2 = Length.getValueOption(unitDestiny);
+                value = (Conversor.convert(unit1, unit2, value));
+            }
+            case "Temperature" -> {
+                Temperature unit1 = Temperature.getValueOption(unitSource);
+                Temperature unit2 = Temperature.getValueOption(unitDestiny);
+                value = (Conversor.convert(unit1, unit2, value));
+            }
+            case "Time" -> {
+                Time unit1 = Time.getValueOption(unitSource);
+                Time unit2 = Time.getValueOption(unitDestiny);
+                value = (Conversor.convert(unit1, unit2, value));
+            }
+            case "Data" -> {
+                Data unit1 = Data.getValueOption(unitSource);
+                Data unit2 = Data.getValueOption(unitDestiny);
+                value = (Conversor.convert(unit1, unit2, value));
+            }
+            default -> {return input;}
+        }
+        return String.valueOf(value);
     }
 }
